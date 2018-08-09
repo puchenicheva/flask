da979ac1d602c5200c847b3c4d0ad118d88ca1f5
Puchenicheva <puchenichevatv@gmail.com>
2018-08-09 10:34:14 +0300

    add say.html, change main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return render_template('index.html')
+@app.route('/say/')
+@app.route('/say/<something>')
+def hello(something=None):
+    return render_template('say.html', something=something)
 
 
 if __name__ == "__main__":
