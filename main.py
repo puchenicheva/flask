785507fc9e3eb8aee2f02c787d670840652e9d08
Puchenicheva <puchenichevatv@gmail.com>
2018-08-15 12:45:18 +0300

    change main.py, task.html, task_completed.html

--- main.py
+++ main.py
@@ -4,27 +4,23 @@ app = Flask(__name__)
 
 
 @app.route('/')
-# def index():
-#     return render_template('index.html')
-#
-#
-# @app.route('/say/')
-# @app.route('/say/<something>/')
-# def hello(something=None):
-#     return render_template('say.html', something=something)
+def index():
+    return render_template('index.html')
 
 
-@app.route("/task/")
+@app.route('/say/')
+@app.route('/say/<something>/')
+def hello(something=None):
+    return render_template('say.html', something=something)
+
+
+@app.route("/task_completed/")
 def task():
     return render_template('task.html')
 
 
-@app.route('/task/', methods=['POST'])
+@app.route('/task_completed/', methods=['POST'])
 def task_post():
-    # name_task = request.form['name_task']
-    # task_description = request.form['task_description']
-    # responsible_for_the_task = request.form['responsible_for_the_task']
-    # task_end_date = request.form['task_end_date']
     return render_template('task_completed.html',
                            name_task=request.form['name_task'],
                            task_description=request.form['task_description'],
