71a233b561907b0b9e3e7b7eb161d94312886114
Puchenicheva <puchenichevatv@gmail.com>
2018-09-19 18:57:37 +0300

    change:.gitignore, main.py, models.py, requirements.txt, task.html, task_completed.html

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-from base import engine, Base
+from models import Task
+from base import Session, engine, Base
 
 app = Flask(__name__)
 
@@ -20,17 +21,30 @@ def task():
     return render_template('task.html')
 
 
+session = Session()
+Base.metadata.create_all(engine)
+
+
 @app.route('/task_completed/', methods=['POST'])
 def task_post():
     name_task = request.form['name_task']
     task_description = request.form['task_description']
     responsible_for_the_task = request.form['responsible_for_the_task']
     task_end_date = request.form['task_end_date']
+    note = Task(name=name_task, description=task_description, responsible=responsible_for_the_task, date=task_end_date)
+    Task.db_addition(note)
     return render_template('task_completed.html',
                            name_task=name_task,
                            task_description=task_description,
                            responsible_for_the_task=responsible_for_the_task,
                            task_end_date=task_end_date)
+
+
+@app.route('/task_completed/', methods=['GET'])
+def task_get():
+    get_task = Task.db.get()
+    return render_template('task_completed.html',
+                           get_task=get_task)
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,18 +1,28 @@
 from sqlalchemy import Column, String, Integer, Date
-from base import Base
+from base import Base, Session
+
+session = Session()
 
 
 class Task(Base):
     __tablename__ = 'task'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    description = Column(String(500), nullable=False)
-    responsible = Column(String(50), nullable=False)
-    date = Column(Date, nullable=False)
+    name = Column(String())
+    description = Column(String())
+    responsible = Column(String())
+    date = Column(Date())
 
-    def __init__(self, name, description, responsible, date):
+    def __init__(self, name=None, description=None, responsible=None, date=None):
         self.name = name
         self.description = description
         self.responsible = responsible
         self.date = date
+
+    def db_addition(self, note):
+        session.commit()
+        session.close()
+        session.add(note)
+
+    def db_get(self):
+        return Task.query.all()
